1b0e5aa1254b4630db87e170e93592781c0d167c
Jpence20 <89040154+Jpence20@users.noreply.github.com>
2022-09-11 23:30:43 -0400

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,3 +6,11 @@ Console.WriteLine("How are you today");
 string mood =Console.ReadLine();
 Console.WriteLine($"Hi {name} so your feeling {mood} today");
 
+
+String[] pot1 = { "Qatar", "Brazil", "Belgium", "France", "Argentina", "Engalnd", "Spain", "Portugal" };
+String[] pot2 = { "Mexico", "Netherlands", "Denmark", "Germany", "Uruguay", "Switzerland", "United States", "Crotia" };
+String[] pot3 = { "Senegal", "IR Iran", "Japan", "Morocco", "Serbia", "Poland", "South Korea", "Tunisia" };
+String[] pot4 = {"Cameroon","Canda","Ecuador","Saudi Arabia","Ghana","Wales","Costa Rica","Australia" };
+
+
+
