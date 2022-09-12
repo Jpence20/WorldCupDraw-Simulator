2806d88e744262cff144820a34a83b49b425bf4a
Jpence20 <89040154+Jpence20@users.noreply.github.com>
2022-09-12 00:00:45 -0400

    update 5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,10 +7,15 @@ string mood =Console.ReadLine();
 Console.WriteLine($"Hi {name} so your feeling {mood} today");
 
 
+
+
 String[] pot1 = { "Qatar", "Brazil", "Belgium", "France", "Argentina", "Engalnd", "Spain", "Portugal" };
 String[] pot2 = { "Mexico", "Netherlands", "Denmark", "Germany", "Uruguay", "Switzerland", "United States", "Crotia" };
 String[] pot3 = { "Senegal", "IR Iran", "Japan", "Morocco", "Serbia", "Poland", "South Korea", "Tunisia" };
 String[] pot4 = {"Cameroon","Canda","Ecuador","Saudi Arabia","Ghana","Wales","Costa Rica","Australia" };
 
+String[] Groups = { "A", "B", "C", "D", "E", "F", "G", "H" };
 
-
+Random rand = new Random();
+int index = rand.Next(pot1.Length);
+Console.WriteLine($"{pot1[index]}");
