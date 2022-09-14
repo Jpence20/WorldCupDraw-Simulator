3986c56fd6674fd3f0811289c5cc2eb586de9564
Jpence20 <89040154+Jpence20@users.noreply.github.com>
2022-09-13 21:43:01 -0400

    World Cup Draw update 6

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 
 Draw();
 void Draw()
-{
+{//pots for the countries
     String[] pot1 = { "Qatar", "Brazil", "Belgium", "France", "Argentina", "Engalnd", "Spain", "Portugal" };
     String[] pot2 = { "Mexico", "Netherlands", "Denmark", "Germany", "Uruguay", "Switzerland", "United States", "Crotia" };
     String[] pot3 = { "Senegal", "IR Iran", "Japan", "Morocco", "Serbia", "Poland", "South Korea", "Tunisia" };
@@ -10,15 +10,15 @@ void Draw()
 
     String[] Groups = { "A", "B", "C", "D", "E", "F", "G", "H" };
 
-
+    // the for loop divides the countries into different groups
     for (int i = 0; i < 8;i++)
     {
         if (i == 1)
         {
             int index = randomSplit(pot1, pot2, pot3, pot4);
-
+            //pot 1 in group A is the only one that will have a fixed country as Qatar is the host nation, so they get put in group A
             Console.WriteLine("Group A");
-            Console.WriteLine($"{pot1[index]},{pot2[index]},{pot3[index]},{pot4[index]}");
+            Console.WriteLine($"{pot1[0]},{pot2[index]},{pot3[index]},{pot4[index]}");
            
         }
         if (i == 2)
@@ -75,6 +75,7 @@ void Draw()
     
 
 }
+//randomly picks a number that is associated with a country in the pot
 int randomSplit(string[] pot1, string[] pot2, string[] pot3, string[] pot4)
 {
     
