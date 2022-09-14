0edfbee28540f2ce2244ab2a759c15a5fdb4c156
Jpence20 <89040154+Jpence20@users.noreply.github.com>
2022-09-14 18:45:15 -0400

    update 9

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,14 +11,20 @@ void Draw()
 
    
 
-    // The for loop divides the countries into different groups
+    // The for loop divides the countries into 8 different groups
+
+
     for (int i = 0; i < 8;i++)
     {
+        //The letters array is used to update the group names
         string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H" };
         
             int index = randomSplit(pot1, pot2, pot3, pot4);
 
+
         //Qatar will always be in group A, since they are the host nations
+
+
         if (i==0)
         {
             
