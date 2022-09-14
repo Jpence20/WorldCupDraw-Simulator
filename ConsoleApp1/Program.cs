18c26da5493ddb143fb12d019fe6af85bd03edfb
Jpence20 <89040154+Jpence20@users.noreply.github.com>
2022-09-14 18:22:20 -0400

    Update 9

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,26 +2,34 @@
 
 Draw();
 void Draw()
-{//pots for the countries
+{
+    //pots for the countries
     String[] pot1 = { "Qatar", "Brazil", "Belgium", "France", "Argentina", "Engalnd", "Spain", "Portugal" };
     String[] pot2 = { "Mexico", "Netherlands", "Denmark", "Germany", "Uruguay", "Switzerland", "United States", "Crotia" };
     String[] pot3 = { "Senegal", "IR Iran", "Japan", "Morocco", "Serbia", "Poland", "South Korea", "Tunisia" };
     String[] pot4 = { "Cameroon", "Canda", "Ecuador", "Saudi Arabia", "Ghana", "Wales", "Costa Rica", "Australia" };
 
-    String[] Groups = { "A", "B", "C", "D", "E", "F", "G", "H" };
+   
 
-    // the for loop divides the countries into different groups
+    // The for loop divides the countries into different groups
     for (int i = 0; i < 8;i++)
     {
         string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H" };
         
             int index = randomSplit(pot1, pot2, pot3, pot4);
 
-
-            //pot 1 in group A is the only one that will have a fixed country as Qatar is the host nation, so they get put in group A
-            Console.WriteLine("Group " + letters[i]);
-            Console.WriteLine($"{pot1[0]},{pot2[index]},{pot3[index]},{pot4[index]}");
-          
+        //Qatar will always be in group A, since they are the host nations
+        if (i==0)
+        {
+            
+            Console.WriteLine($"Group {letters[i]}\r\n");
+            Console.WriteLine($"{pot1[0]}, {pot2[index]}, {pot3[index]}, {pot4[index]}\r\n");
+            i++;
+        }
+        //Fills out the rest of the groups.
+            Console.WriteLine($"Group {letters[i]}\r\n");
+            Console.WriteLine($"{pot1[index]}, {pot2[index]}, {pot3[index]}, {pot4[index]}\r\n");
+        
 
        
     }
@@ -36,6 +44,7 @@ int randomSplit(string[] pot1, string[] pot2, string[] pot3, string[] pot4)
     Random rand = new Random();
     int index = rand.Next(pot1.Length);
     return index;
+   
 }
 
 
