1d823bed3a3fe4922c7539190d4156d616df8005
Jpence20 <89040154+Jpence20@users.noreply.github.com>
2022-09-14 18:05:07 -0400

    Update 7

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,6 @@ Draw();
 void Draw()
 {//pots for the countries
     String[] pot1 = { "Qatar", "Brazil", "Belgium", "France", "Argentina", "Engalnd", "Spain", "Portugal" };
-    
     String[] pot2 = { "Mexico", "Netherlands", "Denmark", "Germany", "Uruguay", "Switzerland", "United States", "Crotia" };
     String[] pot3 = { "Senegal", "IR Iran", "Japan", "Morocco", "Serbia", "Poland", "South Korea", "Tunisia" };
     String[] pot4 = { "Cameroon", "Canda", "Ecuador", "Saudi Arabia", "Ghana", "Wales", "Costa Rica", "Australia" };
@@ -14,60 +13,62 @@ void Draw()
     // the for loop divides the countries into different groups
     for (int i = 0; i < 8;i++)
     {
-        if (i == 1)
+        string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H" };
+        if (i == 0)
         {
-            int index = randomSplit(pot1, pot2, pot3, pot4);
-            //pot 1 in group A is the only one that will have a fixed country as Qatar is the host nation, so they get put in group A
-            Console.WriteLine("Group A");
-            Console.WriteLine($"{pot1[0]},{pot2[index]},{pot3[index]},{pot4[index]}");
-            var countriesList = pot1.ToList();
-            countriesList.Remove("Qatar");
-           
-        }
-        if (i == 2)
-        {
-           
             int index = randomSplit(pot1, pot2, pot3, pot4);
 
-            Console.WriteLine("Group B");
+
+            //pot 1 in group A is the only one that will have a fixed country as Qatar is the host nation, so they get put in group A
+            Console.WriteLine("Group " + letters[i]);
+            Console.WriteLine($"{pot1[0]},{pot2[index]},{pot3[index]},{pot4[index]}");
+          
+           
+        }
+        if (i == 1)
+        {
+            
+            int index = randomSplit(pot1, pot2, pot3, pot4);
+           
+            Console.WriteLine("Group " + letters[i]);
             Console.WriteLine($"{pot1[index]},{pot2[index]},{pot3[index]},{pot4[index]}");
         }
-        if (i == 3)
+        if (i == 2)
         {
             int index = randomSplit(pot1, pot2, pot3, pot4);
 
             Console.WriteLine("Group C");
             Console.WriteLine($"{pot1[index]},{pot2[index]},{pot3[index]},{pot4[index]}");
         }
-        if (i == 4)
+        if (i == 3)
         {
             int index = randomSplit(pot1, pot2, pot3, pot4);
 
             Console.WriteLine("Group D");
             Console.WriteLine($"{pot1[index]},{pot2[index]},{pot3[index]},{pot4[index]}");
         }
-        if (i == 5)
+        if (i == 4)
         {
             int index = randomSplit(pot1, pot2, pot3, pot4);
 
             Console.WriteLine("Group E");
             Console.WriteLine($"{pot1[index]},{pot2[index]},{pot3[index]},{pot4[index]}");
         }
-        if (i == 6)
+        if (i == 5)
         {
             int index = randomSplit(pot1, pot2, pot3, pot4);
 
             Console.WriteLine("Group F");
             Console.WriteLine($"{pot1[index]},{pot2[index]},{pot3[index]},{pot4[index]}");
         }
-        if (i == 7)
+        if (i == 6)
         {
             int index = randomSplit(pot1, pot2, pot3, pot4);
 
             Console.WriteLine("Group G");
             Console.WriteLine($"{pot1[index]},{pot2[index]},{pot3[index]},{pot4[index]}");
         }
-        if (i == 8)
+        if (i == 7)
         {
             int index = randomSplit(pot1, pot2, pot3, pot4);
 
@@ -88,4 +89,5 @@ int randomSplit(string[] pot1, string[] pot2, string[] pot3, string[] pot4)
     int index = rand.Next(pot1.Length);
     return index;
 }
+
 
