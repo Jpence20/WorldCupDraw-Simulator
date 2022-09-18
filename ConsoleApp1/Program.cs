31664f532a32b5e4aa3506687bf61d7ecbbe4779
Jpence20 <89040154+Jpence20@users.noreply.github.com>
2022-09-18 13:31:18 -0400

    Update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,47 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-Draw();
-static void Main(string[] args)
+class program
 {
 
 
+    static void Main(string[] args)
+    {
+        
+        Draw();
 
-}
+    }
 
 
 
     static void Draw()
     {
 
-    String[] pot1 = { "Qatar", "Brazil", "Belgium", "France", "Argentina", "Engalnd", "Spain", "Portugal" };
-    String[] pot2 = { "Mexico", "Netherlands", "Denmark", "Germany", "Uruguay", "Switzerland", "United States", "Crotia" };
-    String[] pot3 = { "Senegal", "IR Iran", "Japan", "Morocco", "Serbia", "Poland", "South Korea", "Tunisia" };
-    String[] pot4 = { "Cameroon", "Canda", "Ecuador", "Saudi Arabia", "Ghana", "Wales", "Costa Rica", "Australia" };
+        String[] pot1 = { "Qatar", "Brazil", "Belgium", "France", "Argentina", "Engalnd", "Spain", "Portugal" };
+        String[] pot2 = { "Mexico", "Netherlands", "Denmark", "Germany", "Uruguay", "Switzerland", "United States", "Crotia" };
+        String[] pot3 = { "Senegal", "IR Iran", "Japan", "Morocco", "Serbia", "Poland", "South Korea", "Tunisia" };
+        String[] pot4 = { "Cameroon", "Canda", "Ecuador", "Saudi Arabia", "Ghana", "Wales", "Costa Rica", "Australia" };
+
+        
+        // The for loop divides the countries into 8 different groups
 
 
-    // The for loop divides the countries into 8 different groups
-
-
-    for (int i = 0; i < 8; i++)
+        for (int i = 0; i < 8; i++)
         {
             //The letters array is used to update the group names
             string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H" };
 
-        
-            int index = Teams.randomSplit(pot1, pot2, pot3, pot4);
-        
+            
+            int index = Teams.randomSplit(pot1);
+            int index2 = Teams.randomSplit(pot2);
+            int index3 = Teams.randomSplit(pot3);
+            int index4 = Teams.randomSplit(pot4);
+
 
             //Qatar will always be in group A, since they are the host nations
-             
+
 
             if (i == 0)
             {
 
                 Console.WriteLine($"Group {letters[i]}\r\n");
-                Console.WriteLine($"{pot1[0]}, {pot2[index]}, {pot3[index]}, {pot4[index]}\r\n");
-           
-            
+                Console.WriteLine($"{pot1[0]}, {pot2[index2]}, {pot3[index3]}, {pot4[index4]}\r\n");
+
+
                 i++;
             }
 
@@ -51,7 +57,7 @@ static void Main(string[] args)
 
             //Fills out the rest of the groups.
             Console.WriteLine($"Group {letters[i]}\r\n");
-            Console.WriteLine($"{pot1[index]}, {pot2[index]}, {pot3[index]}, {pot4[index]}\r\n");
+            Console.WriteLine($"{pot1[index]}, {pot2[index2]}, {pot3[index3]}, {pot4[index4]}\r\n");
 
 
 
@@ -60,30 +66,9 @@ static void Main(string[] args)
 
 
     }
+
    
-public class Teams
-{
-    String[] pot1 = { "Qatar", "Brazil", "Belgium", "France", "Argentina", "Engalnd", "Spain", "Portugal" };
-    String[] pot2 = { "Mexico", "Netherlands", "Denmark", "Germany", "Uruguay", "Switzerland", "United States", "Crotia" };
-    String[] pot3 = { "Senegal", "IR Iran", "Japan", "Morocco", "Serbia", "Poland", "South Korea", "Tunisia" };
-    String[] pot4 = { "Cameroon", "Canda", "Ecuador", "Saudi Arabia", "Ghana", "Wales", "Costa Rica", "Australia" };
-    
-   public static int randomSplit(string[] pot1, string[] pot2, string[] pot3, string[] pot4)
-    {
-
-        Random rand = new Random();
-        int index = rand.Next(pot1.Length);
-        return index;
-
-    }
-    public static void IsDrawn()
-    {
-       
-    }
-
-    
 }
-
 
 
 
---
+++ ConsoleApp1/Teams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Teams
+{
+  
+    public static int randomSplit(string[] pot)
+    {
+
+        Random rand = new Random();
+        int index = rand.Next(pot.Length);
+        return index;
+
+    }
+    public static void IsDrawn()
+    {
+
+    }
+
+
+}
